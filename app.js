544d51613493de5366d4d43ccaa213302dae7228
Ruka Okuyama <rukaokuyama@gmail.com>
2023-05-25 16:56:40 +0900

    added js

---
+++ app.js
@@ -0,0 +1,7 @@
+(function(){
+  let screen = document.querySelector('.screen');
+  let buttons = document.querySelectorAll('.btn');
+  let clear = document.querySelector('.btn-clear');
+  let equal = document.querySelector('.btn-equal');
+
+})();
